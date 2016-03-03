96fd29cef89ca670faefefa90de394716d25aaa9
Anne Nguyen <anne.ma.nguyen@gmail.com>
2016-03-03 06:20:31 -0600

    added answers

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -1,90 +1,115 @@
-Python 2.7.6 (default, Jun 22 2015, 17:58:13) 
-Type "copyright", "credits" or "license" for more information.
+# Based on materials copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
 
-IPython 4.1.1 -- An enhanced Interactive Python.
-?         -> Introduction and overview of IPython's features.
-%quickref -> Quick reference.
-help      -> Python's own help system.
-object?   -> Details about 'object', use 'object??' for extra details.
 
-In [1]: 
-In [2]: 
-In [3]: 
-In [4]: 
-In [5]: remove_adjacent([])
-Out[5]: []
+def match_ends(words):
+    """
+    Given a list of strings, return the count of the number of strings
+    where the string length is 2 or more and the first and last chars
+    of the string are the same.
 
-In [6]: remove_adjacent([1,2,2,3])
-[1]
+    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
+    3
+    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
+    2
+    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
+    1
+    """
 
-In [7]: 
-In [8]: remove_adjacent([1,2,2,3])
-[1, 3]
-
-In [9]: 
-In [10]: remove_adjacent([1,2,2,3])
----------------------------------------------------------------------------
-AttributeError                            Traceback (most recent call last)
-<ipython-input-10-b679b2f2c978> in <module>()
-----> 1 remove_adjacent([1,2,2,3])
-
-/home/amn34/ds/metis/prework/dsp/python/q7_lists.py in remove_adjacent(nums)
-     19     for word in words:
-     20         if len(word) >= 2 and word[0] == word[-1]:
----> 21             counter += 1
-     22     print counter
-     23 
-
-AttributeError: 'list' object has no attribute 'appe'
-
-In [11]: 
-In [12]: remove_adjacent([3,2,3,3,3])
-[3, 2, 3]
-
-In [13]:   File "/home/amn34/ds/metis/prework/dsp/python/q7_lists.py", line 18
     counter = 0
-               
-^
-SyntaxError: invalid syntax
+    for word in words: 
+        if len(word) >= 2 and word[0] == word[-1]:
+            counter += 1
+    print counter
+	
+
+def front_x(words):
+    """
+    Given a list of strings, return a list with the strings in sorted
+    order, except group all the strings that begin with 'x' first.
+    e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
+
+    >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
+    ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
+    >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
+    ['xaa', 'xcc', 'aaa', 'bbb', 'ccc']
+    >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
+    ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+    """
+    words = sorted(words)
+    for word in words:
+        if words[-1].startswith('x'):
+            words.insert(0, words.pop(-1))
+    print words
 
 
-In [14]: linear_merge(['aa','xx','zz'],['bb','cc'])
----------------------------------------------------------------------------
-NameError                                 Traceback (most recent call last)
-<ipython-input-14-68aa0b23a48d> in <module>()
-----> 1 linear_merge(['aa','xx','zz'],['bb','cc'])
+def sort_last(tuples):
+    """
+    Given a list of non-empty tuples, return a list sorted in
+    increasing order by the last element in each tuple.
+    e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
 
-NameError: name 'linear_merge' is not defined
+    >>> sort_last([(1, 3), (3, 2), (2, 1)])
+    [(2, 1), (3, 2), (1, 3)]
+    >>> sort_last([(2, 3), (1, 2), (3, 1)])
+    [(3, 1), (1, 2), (2, 3)]
+    >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
+    [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+    """
+    
+    return sorted(tuples, key = lambda x: x[-1])
 
-In [15]:   File "/home/amn34/ds/metis/prework/dsp/python/q7_lists.py", line 25
-    def front_x(words):
-                       
-^
-SyntaxError: invalid syntax
+
+def remove_adjacent(nums):
+    """
+    Given a list of numbers, return a list where all adjacent equal
+    elements have been reduced to a single element, so [1, 2, 2, 3]
+    returns [1, 2, 3]. You may create a new list or modify the passed
+    in list.
+
+    >>> remove_adjacent([1, 2, 2, 3])
+    [1, 2, 3]
+    >>> remove_adjacent([2, 2, 3, 3, 3])
+    [2, 3]
+    >>> remove_adjacent([3, 2, 3, 3, 3])
+    [3, 2, 3]
+    >>> remove_adjacent([])
+    []
+    """
+
+    if len(nums) ==0: return []
+    else:
+        newlist = [nums[0]]
+        for number in nums:
+            if number != newlist[-1]:
+                newlist.append(number)
+        return newlist
 
 
-In [16]: 
-In [17]: list1=['aa','xx','zz']
+def linear_merge(list1, list2):
+    """
+    Given two lists sorted in increasing order, create and return a
+    merged list of all the elements in sorted order. You may modify
+    the passed in lists. Ideally, the solution should work in "linear"
+    time, making a single pass of both lists.
 
-In [18]: list2=['bb','cc']
+    >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
+    ['aa', 'bb', 'cc', 'xx', 'zz']
+    >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
+    ['aa', 'bb', 'cc', 'xx', 'zz']
+    >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
+    ['aa', 'aa', 'aa', 'bb', 'bb']
+    """
+    
+    mergelist = []
+    while len(list1)>0 and len(list2)>0:
+        if list1[0] < list2[0]:
+            mergelist.append(list1.pop(0))
+        else: mergelist.append(list2.pop(0))
+    mergelist = mergelist + list1 + list2
+    print mergelist
+            
 
-In [19]: linear_merge(list1,list2)
----------------------------------------------------------------------------
-TypeError                                 Traceback (most recent call last)
-<ipython-input-19-6b5b83ffc23f> in <module>()
-----> 1 linear_merge(list1,list2)
 
-/home/amn34/ds/metis/prework/dsp/python/q7_lists.py in linear_merge(list1, list2)
-     18     counter = 0
-     19     for word in words:
----> 20         if len(word) >= 2 and word[0] == word[-1]:
-     21             counter += 1
-     22     print counter
-
-TypeError: 'builtin_function_or_method' object has no attribute '__getitem__'
-
-In [20]: 
-In [21]: linear_merge(['aa','aa'],['aa','bb','bb'])
-['aa', 'aa', 'aa', 'bb', 'bb']
-
-In [22]: 
